722854c00e0407bcd8e6b1f929bc21d934b52e24
sakoua <w37173843@gmail.com>
2022-02-24 13:35:26 +0800

    完成

--- csv轉json/b.py
+++ csv轉json/b.py
@@ -2,6 +2,14 @@ import json
 
 with open('data.json',encoding='utf8') as f:
     data = json.load(f)
-for o in range(1,3):
-    for i in range(len(data)):
-        print() 
+value1=["第一劑","第二劑","第三劑"]
+
+for i in range(len(value1)):
+    ans=0
+    for value in data:
+        if value[value1[i]]!='X':
+            ans+=1
+    print(f"{value1[i]}共打了{ans}個人")
+
+    
+        
